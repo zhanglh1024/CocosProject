08113ef49a2b998c774588713156e673edabbc82
zhanglh <zhanglh@ename.com>
2018-09-17 23:40:18 +0800

    cocos游戏

---
+++ assets/scrits/Star.js
@@ -0,0 +1,55 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // 星星和主角之间的距离小于这个数值时，就会完成收集
+        pickRadius: 0,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+    update: function (dt) {
+        // 每帧判断和主角之间的距离是否小于收集距离
+        if (this.getPlayerDistance() < this.pickRadius) {
+            // 调用收集行为
+            this.onPicked();
+            return;
+        }
+    },
+
+
+    getPlayerDistance: function () {
+        // 根据 player 节点位置判断距离
+        var playerPos = this.game.player.getPosition();
+        // 根据两点位置计算两点之间距离
+        var dist = this.node.position.sub(playerPos).mag();
+        return dist;
+    },
+
+    onPicked: function() {
+        // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+        this.game.spawnNewStar();
+
+        //调用 Game 脚本的得分方法
+        this.game.gainScore();
+
+
+        // 然后销毁当前星星节点
+        this.node.destroy();
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
